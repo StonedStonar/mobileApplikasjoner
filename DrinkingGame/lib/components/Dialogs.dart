09292cca8fae45ba02a6e38e052faa0aecd4d962
stonedstonar <steinarhjellem@gmail.com>
2022-10-06 09:17:44 +0200

    Fixed basic layout of the login page

---
+++ DrinkingGame/lib/components/Dialogs.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+///Shows an alert that is defined
+///[context] the build context
+///[title] the title of the dialog
+///[content] the content of the dialog
+///[defaultActionText] the default action text. Like ok
+///Returns true if the user has pressed Ok.Otherwise false.
+Future<bool?> showAlertDialog(BuildContext context,
+    {required String title,
+    required String content,
+    required String defaultActionText,
+    String? cancelActionText}) {
+  if (!Platform.isIOS) {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: <Widget>[
+          if (cancelActionText != null)
+            TextButton(
+              child: Text(cancelActionText),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+          TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text(defaultActionText)),
+        ],
+      ),
+    );
+  } else {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: <Widget>[
+          if (cancelActionText != null)
+            CupertinoDialogAction(
+              child: Text(cancelActionText),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+          CupertinoDialogAction(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text(defaultActionText)),
+        ],
+      ),
+    );
+  }
+}
+
+///Shows an exception alert dialog.
+///[context] the build context.
+///[title] the tile of the alert.
+///[exception] the exception
+///Returns a dialog with the exception as error.
+Future<void> showExceptionAlertDialog(BuildContext context,
+    {required String title, required Exception exception}){
+  return showAlertDialog(
+      context,
+      title: title,
+      content: _getExceptionMessage(exception),
+      defaultActionText: "Ok");
+}
+
+///Gets the exception message from the exception
+///[exception] the exception itself.
+///Returns the exception message.
+String _getExceptionMessage(Exception exception){
+  String errorString;
+  if(exception is FirebaseException){
+    errorString = exception.message.toString();
+  }else{
+    errorString = exception.toString();
+  }
+  return errorString;
+}
--- DrinkingGame/lib/pages/LoginPage.dart
+++ DrinkingGame/lib/pages/LoginPage.dart
@@ -3,53 +3,87 @@ import 'package:drinkinggame/components/buttons/SignInButton.dart';
 import 'package:drinkinggame/components/buttons/SocialSignInButton.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
-
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
-
+class _LoginPageState extends State<LoginPage> {
+  bool _isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: makeLoginAppBar(),
+      backgroundColor: Colors.grey.shade100,
       body: makePageContent(context),
     );
   }
 
-  Widget makePageContent(BuildContext context){
+  ///Makes the main content of the page.
+  /// [context] the build context
+  Widget makePageContent(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        SocialSignInButton(assetName: "images/google-logo.png", text: "Login with google", onPressed: () => _loginWithGoogle(context)),
-        SignInButton(text: "Sign in with email", onPressed: () => _loginWithEmail(context)),
+        SocialSignInButton(
+            assetName: "images/google-logo.png",
+            text: "Login with google",
+            onPressed: () => _isLoading ? null : _loginWithGoogle(context)),
+        SignInButton(
+            text: "Sign in with email",
+            onPressed: () => _isLoading ? null : _loginWithEmail(context)),
         Padding(
-            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-            child: Text(
-                "Don't want to login?",
-                style: TextStyle(
-                    fontSize: 20,
-                ),
+          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+          child: Text(
+            "Don't want to login?",
+            style: TextStyle(
+              fontSize: 20,
             ),
+          ),
         ),
-        SignInButton(text: "Go anonyomous", onPressed: () => _loginWithAnonymous(context)),
-
-
+        SignInButton(
+            text: "Go anonyomous",
+            onPressed: () => _isLoading ? null : _loginWithAnonymous(context)),
       ],
     );
   }
-  
-  void _loginWithGoogle(BuildContext context){
-    
+
+  void _loginWithGoogle(BuildContext context) {
+    try {
+      _isLoading = true;
+    } on Exception catch (e) {
+      print(e.toString());
+    } finally {
+      _setLoadingState(false);
+    }
   }
-  
-  void _loginWithEmail(BuildContext context){
-    
+
+  void _loginWithEmail(BuildContext context) {
+    try {
+      _isLoading = true;
+    } on Exception catch (e) {
+      print(e.toString());
+    } finally {
+      _setLoadingState(false);
+    }
   }
-  
-  void _loginWithAnonymous(BuildContext context){
-    
+
+  void _loginWithAnonymous(BuildContext context) {
+    try {
+      _isLoading = true;
+    } on Exception catch (e) {
+      print(e.toString());
+    } finally {
+      _setLoadingState(false);
+    }
   }
-  
+
+  ///Sets the loading state to a new value.
+  ///[loadingState] the new loading state.
+  void _setLoadingState(bool loadingState) {
+    setState(() => _isLoading = loadingState);
+  }
 }
