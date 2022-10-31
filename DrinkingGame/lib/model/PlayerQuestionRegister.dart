972f4a17f3e9a2bc848cdb5f4c0ef694846bf81a
trine2301 <stavertrine@gmail.com>
2022-10-31 15:31:26 +0100

    added classes PlayerQuestionRegister and PlayerQuestion

---
+++ DrinkingGame/lib/model/PlayerQuestionRegister.dart
@@ -0,0 +1,10 @@
+
+
+///Representes a register of questions made by player.
+class PlayerQuestionRegister {
+  List<PlayerQuestion> playerQuestions;
+
+  PlayerQuestion getRandomQuestionForPlayer(player) {}
+
+
+}
