e8edbd73bdbd07d7b041a995f58a7a0cc3bba9d9
trine2301 <stavertrine@gmail.com>
2022-11-02 11:12:02 +0100

    Modified PlayerQuestions

--- DrinkingGame/lib/model/PlayerQuestion.dart
+++ DrinkingGame/lib/model/PlayerQuestion.dart
@@ -1,3 +1,34 @@
-class PlayerQuestion {
 
+
+import 'package:drinkinggame/model/Questions.dart';
+import 'package:drinkinggame/services/enums/Response.dart';
+
+import 'Player.dart';
+
+class PlayerQuestion extends Questions {
+  Response? response;
+  Player? madeBy;
+  Player? respondant;
+
+  Response? getResponse() {
+    return response;
+  }
+
+  Player? getMadeBy() {
+    return madeBy;
+  }
+
+  Player? getRespondant() {
+    return respondant;
+  }
+
+  bool? isUSed() {
+
+  }
+
+  void answerQuestion(Response response, Player player) {
+    this.response = response;
+    this.madeBy = player;
+    print("Answer to question: " + response.toString(), );
+  }
 }
