44c6bf6488d4a8cf83f8ad4960b6c0b3c9d8f635
trine2301 <stavertrine@gmail.com>
2022-11-09 17:59:06 +0100

    Added the getRandomQuestionForPlayer(player)-method

--- DrinkingGame/lib/model/registers/PlayerQuestionRegister.dart
+++ DrinkingGame/lib/model/registers/PlayerQuestionRegister.dart
@@ -1,12 +1,21 @@
+
+import 'dart:math';
+
 import 'package:drinkinggame/model/Player.dart';
-import 'package:drinkinggame/model/StoreableItem.dart';
-import 'package:drinkinggame/model/registers/QuestionRegister.dart';
 import 'package:drinkinggame/model/questions/PlayerQuestion.dart';
+import 'package:drinkinggame/model/questions/Question.dart';
+import 'package:drinkinggame/model/registers/QuestionRegister.dart';
 
 ///Representes a register of questions made by player.
 class PlayerQuestionRegister extends QuestionRegister {
 
+  ///Makes an instance of playerQuestions.
+  ///[playerQuestions] the playerQuestions.
+  PlayerQuestionRegister({required List<PlayerQuestion> playerQuestions}) : this.playerQuestions = playerQuestions;
+
+  List<PlayerQuestion> playerQuestions;
   bool isRegisterEmpty = false;
+
 
   ///Check to see if there are questions in the register or not.
   ///Returns true if register has questions.
@@ -20,5 +29,13 @@ class PlayerQuestionRegister extends QuestionRegister {
 
 
 
-  //PlayerQuestion getRandomQuestionForPlayer(player)
+  ///Returns a random question for a specified player
+  ///TODO fix this: only generates a random question, not for a specified user.
+  Question getRandomQuestionForPlayer(player) {
+    if (hasQuestions()) {
+      return (getRegisterItems().toList()..shuffle()).first;
+    } else {
+      throw Exception("There is no elements in the list"); //prob npt necessary, since "first" throws an exception if list is empty.
+    }
+  }
 }
