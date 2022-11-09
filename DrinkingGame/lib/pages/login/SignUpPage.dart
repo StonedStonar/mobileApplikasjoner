c3c1190a838f5f72077ef572524dbcfd2ed8faae
Jones <kraftsghy@gmail.com>
2022-11-09 16:45:47 +0100

    Refactored signup & loginpage

--- DrinkingGame/lib/pages/login/SignUpPage.dart
+++ DrinkingGame/lib/pages/login/SignUpPage.dart
@@ -1,21 +1,13 @@
-import 'package:drinkinggame/components/AppBars.dart';
-import 'package:drinkinggame/components/forms/LoginForm.dart';
-import 'package:drinkinggame/components/forms/SignUpForm.dart';
-import 'package:drinkinggame/providers/signup_login_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../components/forms/AuthenticationForm.dart';
 
 class SignUpPage extends ConsumerWidget {
   SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final displayLoginOrSignUpPage = ref.watch(signUpOrLoginProvider);
-    return Scaffold(
-      appBar: makeBasicAppbar(displayLoginOrSignUpPage ? "Login" : "Register user"),
-      body: SingleChildScrollView(
-          child: displayLoginOrSignUpPage ? LoginForm() : SignUpForm(),
-      )
-    );
+    return AuthenticationForm();
   }
 }
