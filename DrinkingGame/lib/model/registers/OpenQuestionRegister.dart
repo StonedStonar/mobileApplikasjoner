48dcb66668f8c2b61d7eecc14d2f5551ad4730f2
trine2301 <stavertrine@gmail.com>
2022-11-10 10:53:52 +0100

    modified openQuestionsRegister

--- DrinkingGame/lib/model/registers/OpenQuestionRegister.dart
+++ DrinkingGame/lib/model/registers/OpenQuestionRegister.dart
@@ -1,10 +1,10 @@
-import 'package:drinkinggame/model/StoreableItem.dart';
-import 'package:drinkinggame/model/registers/QuestionRegister.dart';
-import 'package:drinkinggame/model/registers/Register.dart';
 
-///Represents a register for open questions.
+import 'package:drinkinggame/model/registers/QuestionRegister.dart';
+
+///Represents a register for open questions which every player can answer.
 class OpenQuestionRegister extends QuestionRegister{
 
+  ///Checks if the question is already used.
   @override
   bool hasQuestions() {
     return super.getRegisterItems().any((openQuestion) => !openQuestion.isUsed());
