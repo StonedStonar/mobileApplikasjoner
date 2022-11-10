4a63233935e2bc3d638935e205f8408e0bd2e6e5
trine2301 <stavertrine@gmail.com>
2022-11-10 12:55:17 +0100

    added some logic to StatementGame and finished OpenQuestionsRegister

--- DrinkingGame/lib/model/games/StatementGame.dart
+++ DrinkingGame/lib/model/games/StatementGame.dart
@@ -1,33 +1,36 @@
 
 
 import 'package:drinkinggame/model/games/Game.dart';
-import 'package:drinkinggame/model/questions/OpenQuestion.dart';
+import 'package:drinkinggame/model/questions/PlayerQuestion.dart';
 import 'package:drinkinggame/model/registers/OpenQuestionRegister.dart';
+import 'package:drinkinggame/model/registers/QuestionRegister.dart';
 import 'package:drinkinggame/model/registers/Register.dart';
 
 ///Represents a game where player answer yes or no to a statement.
 class StatementGame extends Game {
 
   ///Makes an instance of
-  StatementGame({required super.rules, required super.gameName, required super.shortDescription, required List<OpenQuestion> openQuestion})
-  : _openQuestion = openQuestion;
+  StatementGame({required super.rules, required super.gameName, required super.shortDescription});
 
-  List<OpenQuestion> _openQuestion;
-  OpenQuestionRegister _openQuestionRegister;
+  OpenQuestionRegister _openQuestionRegister = OpenQuestionRegister();
 
 
   ///Gets an open question
   @override
-  List<OpenQuestion> getOpenQuestions(){
-  return _openQuestion;
+  OpenQuestionRegister getOpenQuestions(){
+  return _openQuestionRegister;
   }
 
-
+  ///Gets the game register.
+  ///Returns a map with the game register.
   @override
   Map<String, dynamic> toMap() {
-    // TODO: implement toMap
-    throw UnimplementedError();
+    Map<String, dynamic> map = getGameDetails();
+    _openQuestionRegister.add(PlayerQuestion(questionText: "Question 1", questionId: "1", response: "Response1", madeBy: "madeBy1"));
+    _openQuestionRegister.add(PlayerQuestion(questionText: "Question 2", questionId: "2", response: "Response2", madeBy: "madeBy2"));
+    return map;
   }
+
 
   @override
   Register getGameRegister() {
--- DrinkingGame/lib/model/registers/OpenQuestionRegister.dart
+++ DrinkingGame/lib/model/registers/OpenQuestionRegister.dart
@@ -4,14 +4,20 @@ import 'package:drinkinggame/model/registers/QuestionRegister.dart';
 ///Represents a register for open questions which every player can answer.
 class OpenQuestionRegister extends QuestionRegister{
 
-  ///Checks if the question is already used.
-  @override
-  bool hasQuestions() {
+  bool used = false;
+
+  /// Checks to see if a question is used.
+  /// returns true if question is used.
+  bool isUsed() {
     return super.getRegisterItems().any((openQuestion) => !openQuestion.isUsed());
+
   }
 
-
-
-
-
+  @override
+  bool hasQuestions() {
+    if(getRegisterItems().isNotEmpty){
+      used = true;
+    }
+    return used;
+  }
 }
