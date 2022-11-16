6437ce5b4c664e31d375b63afd1a982c072a80b7
Jones <jonesma@ntnu.no>
2022-11-16 16:07:17 +0100

    Improved styling and layout on T/D

--- DrinkingGame/lib/pages/gamePages/truthordare/PlayerInputPage.dart
+++ DrinkingGame/lib/pages/gamePages/truthordare/PlayerInputPage.dart
@@ -31,59 +31,61 @@ class _PlayerInputPageState extends ConsumerState<PlayerInputPage> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 50, 20, 50),
-          child: _buildPlayerInputField(),
-        )
+    return SingleChildScrollView(
+      child: Padding(
+            padding: const EdgeInsets.fromLTRB(5, 65, 5, 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: _buildPlayerInputField(),
+            )
+
       ),
     );
   }
 
-  Widget _buildPlayerInputField() {
-    return Column(
-      children: [
-        const Text(
-          "Write in the names of the players",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-        ),
+   List<Widget> _buildPlayerInputField() {
+    ///TODO: REMOVE
+    ///_fillPlayerListWithTestPlayers();
+    return [
+        _buildHeadText("Write in the names of \n the players", 30),
         const SizedBox(height: 20),
         _buildTextFieldWithButton(),
+        const SizedBox(height: 40),
+        _buildHeadText("Players in game", 25),
         _buildAddedPlayersList(),
-        _buildNextPageButton()
-      ],
-    );
+         Padding(padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+         child: _buildNextPageButton())
+      ];
   }
 
   Widget _buildTextFieldWithButton() {
     bool playerErrorText = _submitted && !widget.usernameValidator.isValid(_playerInput);
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-            child: buildPlayerTextField(_playerInputController, _updateState, playerErrorText, _addPlayerToList),
+    return Container(
+      margin: EdgeInsets.fromLTRB(55, 0, 0, 0),
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+              child: buildPlayerTextField(_playerInputController, _updateState, playerErrorText, _addPlayerToList),
           ),
-        ),
-        TextButton(onPressed: _addPlayerToList, child: Column(
-          children: const [
-            SizedBox(height: 10),
-            Icon(CupertinoIcons.add, size: 30,),
-            Text("Add", style: TextStyle(fontSize: 14),)
-          ],
-        ))
-      ],
+          TextButton(onPressed: _addPlayerToList, child: Column(
+            children: const [
+              SizedBox(height: 11),
+              Icon(CupertinoIcons.add, size: 30,),
+              Text("Add", style: TextStyle(fontSize: 14),)
+            ],
+          ))
+        ],
+      ),
     );
   }
 
   Widget _buildAddedPlayersList() {
     List<Player> players = playerRegister.getPlayers();
     return Padding(
-          padding: const EdgeInsets.fromLTRB(25, 10, 25, 25),
+          padding: const EdgeInsets.fromLTRB(30, 10, 10, 25),
           child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -92,34 +94,42 @@ class _PlayerInputPageState extends ConsumerState<PlayerInputPage> {
                 for (Player player in players)
                   Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                      Text(player.getPlayerName(), style: TextStyle(fontSize: 20)),
-                      TextButton(onPressed: () => _removePlayerFromList(player), child: Column(
-                        children: const [
-                          SizedBox(height: 5),
-                          Icon(CupertinoIcons.minus, size: 30,),
-                        ],
-                      ))
+                        SizedBox(width: 35),
+                        Text(player.getPlayerName(), style: TextStyle(fontSize: 20)),
+                          TextButton(onPressed: () => _removePlayerFromList(player), child: Column(
+                          children: const [
+                            SizedBox(height: 5),
+                            Icon(CupertinoIcons.minus, size: 30,),
+                          ],
+                        )),
                     ],),
               ],
             ),
           )
-
     );
   }
 
   Widget _buildNextPageButton() {
     return CustomElevatedButton(widget: Text(
-        "Next page",
-        style: TextStyle(fontSize: 22),
-      ),
-        borderRadius: 10,
-        onPressed: () {},
-        color: const Color(0xFF000434),
-      );
+            "Next page",
+            style: TextStyle(fontSize: 22),
+          ),
+            borderRadius: 10,
+            onPressed: () {},
+            color: const Color(0xFF000434),
+    );
   }
-
+  
+  Widget _buildHeadText(String text, double fontSize) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.w600),
+    );
+  }
+  
   void _addPlayerToList() {
     Player player = Player(playerID: playerId, playerName: _playerInput);
     playerRegister.addPlayer(player);
@@ -134,5 +144,14 @@ class _PlayerInputPageState extends ConsumerState<PlayerInputPage> {
 
   void _updateState() {
     setState(() {});
+  }
+
+  ///TODO: REMOVE
+  void _fillPlayerListWithTestPlayers() {
+    Player player1 = Player(playerID: 1, playerName: "Fuckyou");
+    Player player2 = Player(playerID: 2, playerName: "Pikachu");
+    playerRegister.addPlayer(player1);
+    playerRegister.addPlayer(player2);
+    _updateState();
   }
 }
