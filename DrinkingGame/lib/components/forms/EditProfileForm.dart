9eb122a8df0c9ac528f5786bc91983098065fadd
Jones <jonesma@ntnu.no>
2022-12-01 20:47:49 +0100

    Edit profile is done(?) needs testing

--- DrinkingGame/lib/components/forms/EditProfileForm.dart
+++ DrinkingGame/lib/components/forms/EditProfileForm.dart
@@ -9,14 +9,36 @@ import '../AppBars.dart';
 import '../buttons/CustomElevatedButton.dart';
 
 
-class EditUsernameForm extends ConsumerStatefulWidget with UsernamePasswordAndEmailValidators {
-  EditUsernameForm({Key? key}) : super(key: key);
+class EditProfileForm extends ConsumerStatefulWidget with UsernamePasswordAndEmailValidators {
+  EditProfileForm({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<EditUsernameForm> createState() => EditUsernameFormState();
+  ConsumerState<EditProfileForm> createState() => EditProfileFormState();
 }
 
-class EditUsernameFormState extends ConsumerState<EditUsernameForm>{
+class EditProfileFormState extends ConsumerState<EditProfileForm>{
+
+  ///TexteditingControllers for getting user input
+  final TextEditingController _editUsernameController = TextEditingController();
+  final TextEditingController _editEmailController = TextEditingController();
+  final TextEditingController _editPasswordController = TextEditingController();
+  final TextEditingController _confirmationPasswordController = TextEditingController();
+
+
+  final FocusNode _editUsernameFocusNode = FocusNode();
+  final FocusNode _editEmailFocusNode = FocusNode();
+  final FocusNode _editPasswordFocusNode = FocusNode();
+  final FocusNode _confirmPasswordFocusNode = FocusNode();
+
+
+  ///Simplified getters for the user input
+  String get _editUsername => _editUsernameController.text;
+  String get _editEmail => _editEmailController.text;
+  String get _editPassword => _editPasswordController.text;
+  String get _editConfirmationPassword => _confirmationPasswordController.text;
+
+  ///True when the user has clicked submit atleast once
+  bool _submitted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,17 +57,6 @@ class EditUsernameFormState extends ConsumerState<EditUsernameForm>{
     );
   }
 
-  ///TexteditingControllers for getting user input
-  final TextEditingController _editUsernameController = TextEditingController();
-  
-  final FocusNode _editUsernameFocusNode = FocusNode();
-
-  ///Simplified getters for the user input
-  String get _editUsername => _editUsernameController.text;
-
-  ///True when the user has clicked submit atleast once
-  bool _submitted = false;
-
   List<Widget> _buildEditUsernameWidget() {
     bool usernameErrorText = _submitted && !widget.usernameValidator.isValid(_editUsername);
     return [
@@ -59,6 +70,32 @@ class EditUsernameFormState extends ConsumerState<EditUsernameForm>{
           false,
           _submit,
           "new_username"),
+
+      ///Submit form button
+      CustomElevatedButton(widget: Text(
+        "Change username",
+        style: TextStyle(fontSize: 22),
+      ),
+        borderRadius: 10,
+        onPressed: _submit,
+        color: const Color(0xFF000434),
+      )
+    ];
+  }
+
+  List<Widget> _buildEditEmailWidget() {
+    bool usernameErrorText = _submitted && !widget.usernameValidator.isValid(_editUsername);
+    return [
+
+      buildUsernameTextField(_editUsernameController,
+          _editUsernameFocusNode,
+          _updateState,
+          "New email",
+          "Your new email",
+          usernameErrorText,
+          false,
+          _submit,
+          "new_email"),
 
       ///Submit form button
       CustomElevatedButton(widget: Text(
